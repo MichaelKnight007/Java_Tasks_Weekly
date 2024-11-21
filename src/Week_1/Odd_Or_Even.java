9ca56d6c2ecc5593b5607704ccf7b4adfeaee241
Ersin <michael.knight@cydeo.com>
2024-11-21 17:38:37 +0300

    Created JavaTasksWeekly project

---
+++ src/Week_1/Odd_Or_Even.java
@@ -0,0 +1,27 @@
+package Week_1;
+
+public class Odd_Or_Even {
+
+    /*
+    Write  a method which can identifies given number is even or odd
+EX:
+identify(5) ->  "Odd"
+identify(6) ->  "Even"
+
+     */
+
+    public static void main(String[] args) {
+
+        int num_1 = 5;
+        int num_2 = 6;
+
+        System.out.println("identify Odd Even: " + num_1 + " is " + identifyOddEven(num_1));
+        System.out.println("identify Odd Even: " + num_2 + " is " + identifyOddEven(num_2));
+
+    }
+
+    public static String identifyOddEven(int n) {
+        return n % 2 == 0 ? "Even" : "odd";
+    }
+
+}
